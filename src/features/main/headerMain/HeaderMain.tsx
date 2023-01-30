b324084b3c2164bc702fa16c4b671064bad8c892
Askerko Alexandr <askerko50@gmail.com>
2023-01-30 23:58:06 +0300

    fix(animation modal header)

--- src/features/main/headerMain/HeaderMain.tsx
+++ src/features/main/headerMain/HeaderMain.tsx
@@ -7,16 +7,22 @@ export const HeaderMain = () => {
     const openModal = () => {
         setView(true)
     }
-    const closeModal=()=>{
+    const closeModal = () => {
         setView(false)
     }
+    const showModal=view?s.containerModal:`${s.containerModal} ${s.noModal}`
     return (
         <div className={s.container}>
 
             <div className={s.h1}>Создаем сайты лучше <br/> <span className={s.text}>чем у конкурентов</span>
-                <p className={s.paragrah}>Создаем по-настоящему эффективные сайты: внедряем актуальные<br/> IT разработки, уделяем максимум внимание дизайну и юзабилити</p>
+                <p className={s.paragrah}>Создаем по-настоящему эффективные сайты: внедряем актуальные<br/> IT
+                    разработки, уделяем максимум внимание дизайну и юзабилити</p>
                 <button className={s.button} onClick={openModal}>ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ</button>
-                {view && <div className={s.formContainer} ><FormComponent closemodal={closeModal}  /></div>}
+
+                    <div className={showModal}>
+                        <div className={s.formContainer}><FormComponent closemodal={closeModal}/></div>
+                    </div>
+
             </div>
             <div className={s.animation}>
                 ANIMATION
