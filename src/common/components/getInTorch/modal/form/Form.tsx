527ba5f5b993c12c1d3479dc5f63ec02216bd4cd
alexAskerko <askerko50@gmail.com>
2023-02-03 20:15:20 +0300

    fix:modal

--- src/common/components/getInTorch/modal/form/Form.tsx
+++ src/common/components/getInTorch/modal/form/Form.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import s from "./Form.module.scss";
+import imgLogo from "../../../../../assets/img/logoModal.jpg";
 
-type FormComponentPropsType={
-    closemodal:()=>void
+type FormComponentPropsType = {
+    closemodal: () => void
 }
-export const FormComponent:React.FC<FormComponentPropsType>= ({closemodal}) => {
-    return (
+export const FormComponent: React.FC<FormComponentPropsType> = ({closemodal}) => {
+
+    const logo = {
+            backgroundImage: 'url(' + imgLogo + ')',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+           backgroundRepeat:'no-repeat'
+        }
+
+        return(
         <form className={s.form}>
+            <div className={s.logo} style={logo}> </div>
             <div className={s.closeModal} onClick={closemodal}>x</div>
             <div className={s.name}>Заявка на обратный звонок</div>
 
             <input className={s.input} name="Name" id="name" placeholder="Ваше имя"/>
             <input className={s.input} name="Phone" id="email" placeholder="Номер телефона"/>
 
-            <p className={s.textareaWrapper} >Нажимая на кнопку «Отправить», вы даете свое согласие на обработку персональных данных
+            <p className={s.textareaWrapper}>Нажимая на кнопку «Отправить», вы даете свое согласие на обработку
+                персональных данных
                 в соответствии с целями указанными в Политике обработки персональных данных</p>
 
             <button className={s.button} type={"submit"} name={'send message'}>ОТПРАВИТЬ</button>
