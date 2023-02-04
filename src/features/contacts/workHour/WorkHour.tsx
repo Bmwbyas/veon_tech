f01307b0d7bd9e9570260c7a0c43b6e93e207b11
alexAskerko <askerko50@gmail.com>
2023-02-04 15:07:10 +0300

    fix:contacts

--- src/features/contacts/workHour/WorkHour.tsx
+++ src/features/contacts/workHour/WorkHour.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from "./WorkHour.module.scss"
 import {workhourData} from "../data";
 
 export const WorkHour = () => {
+// useEffect(()=>{
+    const getWeekDay=():string=> {
+        const date=new Date()
+        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+        return days[date.getDay()];
+
+    }
+
+// },[])
+
     return (
         <div>
             <div className={s.circleContainer}>
@@ -17,10 +28,12 @@ export const WorkHour = () => {
             <div className={s.workHourContainer}>
                 <div className={s.headrerWork}>Время работы:</div>
                 <div className={s.hourWorkWrap}>
-                    {workhourData.map((w, index) => <div key={index} className={s.workHourItemContainer}>
+                    {workhourData.map((w, index) =>{
+                        const classNameContainer=getWeekDay()===w.data?s.workHourItemContainer + ' ' +s.active: s.workHourItemContainer
+                        return <div key={index} className={classNameContainer}>
                         <div>{w.day}</div>
                         <div>{w.time}</div>
-                    </div>)}
+                    </div>})}
                 </div>
             </div>
 
