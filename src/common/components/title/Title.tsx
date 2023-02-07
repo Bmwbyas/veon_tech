bb1ab3718fb304a7186cecade3e60982dd9b95f2
alexAskerko <askerko50@gmail.com>
2023-02-07 11:00:20 +0300

    fix:title img

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import s from './Title.module.scss'
+import case2 from "../../../assets/img/imgTitle.png";
 
 type TitleType = {
     header: string
     desc: string
 }
 export const Title: React.FC<TitleType> = ({header, desc}) => {
-
+    const imgBG = {
+        backgroundImage: 'url(' + case2 + ')',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+    }
     return (
-        <div className={s.wrap}>
-            <div className={s.titleContainer}>
-                <div className={s.titleWrapper}>
+        <div className={s.wrap}  style={imgBG}>
+            <div className={s.titleContainer} >
+                <div className={s.titleWrapper} >
                     <h2 className={s.titleV}>{header}</h2>
                     <div className={s.breadcrumb}>{desc}</div>
                 </div>
