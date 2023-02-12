e83026f9a1a20b280f2805225803830b3e17bfa3
alexAskerko <askerko50@gmail.com>
2023-02-12 21:57:03 +0300

    fix:adaptiveMobile webdev

--- src/features/services/webSupport/CostService/CostService.tsx
+++ src/features/services/webSupport/CostService/CostService.tsx
@@ -1,47 +1,50 @@
 import React from 'react';
 import s from './CostService.module.scss'
-import logo from "../../../../assets/img/hr-girl.jpg";
+import absolute from "../../../../assets/img/imgSupport/absolute-maintenance.png";
+import advice from "../../../../assets/img/imgSupport/advanced-maintenance.png";
+import optimal from "../../../../assets/img/imgSupport/optimal-maintenance.png";
+import {Button} from "../../../../common/components/Button/Button";
 
 // @ts-ignore
 import {Carousel} from '3d-react-carousal';
-import {Card} from "./card/Card";
+
+import Contact from "../../../main/contact/Contact";
+
 
 export const CostService = ({}) => {
-    const resetSlider = (index:any) => {
-        return index;
-    }
-    const sliderSettings = {
+    let slides = [
+        <div>
+            <img src={absolute} alt="1"/>
+            <div>Абсолютный</div>
+            <div>Абсолютный</div>
+            <div><Button name={'Заказать'}/></div>
+        </div>,
+        <div>
+            <img src={advice} alt="2"/>
+            <div>dfdgvdfgsdf</div>
+            <div><Button name={'Заказать'}/></div>
+        </div>,
+        <div>
+            <img src={optimal} alt="3"/>
+            <div>dfdgvdfgsdf</div>
+            <div><Button name={'Заказать'}/></div>
+        </div>,
+    ];
 
-        infinite: true,
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        accessibility: true,
-        // variableWidth: true
-
-    }
-    const costData=[
-        <Card imagen={logo}/>,
-        <Card imagen={logo}/>,
-        <Card imagen={logo}/>,
-
-
-            ]
     return (
         <div className={s.container}>
-            <div>
-                <div className={s.h1}>Гибкие и Прозрачные Цены</div>
-                <div className={s.desc}>Lipsum dolor sit amet, consectetur adipisicing elit. sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                </div>
 
-
-                </div>
+            <div className={s.h1}>Гибкие и Прозрачные Цены</div>
+            <div className={s.desc}>Lipsum dolor sit amet, consectetur adipisicing elit. sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
             </div>
+            <div className={s.sliderContainer}>
+                <Carousel  slides={slides} autoplay={false} arrows={true}  />
+            </div>
+           <Contact/>
+
+        </div>
+
 
     );
 };
